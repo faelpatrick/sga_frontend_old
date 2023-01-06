bf6ac18fd7c5063db5be0b4ebe2e9bb2af19373f
faelpatrick <faelpatrick@gmail.com>
2023-01-06 19:29:12 +0000

    first commit

--- src/pages/WelcomePage/index.jsx
+++ src/pages/WelcomePage/index.jsx
@@ -5,14 +5,13 @@ const WelcomePage = () => {
     return (
         <div className="welcome_page">
             <h1><p>SGA - Sistema de Gestão de Academico</p></h1>
-            <h2><p>Gestão de:
+            <h2>Gestão de:</h2>
                 <ul>
                     <li>Cursos</li>
                     <li>Disciplinas</li>
                     <li>Professores</li>
-                    <li>Aluno                   </li>
-
-                </ul> </p></h2>
+                    <li>Aluno</li>
+                </ul>
             <h3><p>Desenvolvido por: Rafael Patrick de Souza</p></h3>
         </div>
     )
